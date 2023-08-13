73ba166e8e5018f06d46efc5b3618e39825e0505
Anurag Kumar <anuraganu202016@gmail.com>
2023-08-14 00:11:25 +0530

    Connected responses with MongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Page1 from './Page1'
 import { Route , Routes } from 'react-router-dom'
 import ShowQ from './ShowQ'
 import EndPage from './EndPage'
+import ThankYou from './ThankYou'
+
 export default function App() {
   return (
     <Routes>
@@ -10,6 +12,7 @@ export default function App() {
       <Route path='/' element={<Page1></Page1>}></Route>
       <Route path='/ques' element={<ShowQ></ShowQ>}></Route>
       <Route path='/end' element={<EndPage></EndPage>}></Route>
+      <Route path='/thankyou' element={<ThankYou></ThankYou>}></Route>
 
     </Routes>
   )
--- src/EndPage.jsx
+++ src/EndPage.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import "./EndPage.css"
 import img1 from './image/background6.jpg'
+import { useNavigate } from 'react-router-dom';
 
 export default function EndPage() {
+  const navigate = useNavigate();
+
+  const responsearr = localStorage.getItem("userans");
+  const username = localStorage.getItem("username");
+  const email = localStorage.getItem("useremail");
+  const submitresponse =()=>
+  {
+    fetch("http://localhost:8000/submit",
+    {
+      method :"post",
+      headers:{ "Content-Type":"application/json"},
+      body: JSON.stringify({
+
+       username:username,
+       email:email,
+       response:responsearr
+      })
+  
+    }).then(res => res.json()).then(data => {
+    
+        if(data.message)
+        {
+          console.log("success")
+          localStorage.setItem("submitted","true");
+          navigate("/thankyou");              
+        }
+        else{
+            console.log("error saving data");    
+      }
+
+    });
+
+
+  }
   return (<>
   <img src={img1} alt = ""/>
     <div className='tyb'>
-      <h1 className='ty'> Your responses have been Recorded.<span id='tnq'><br></br>Thank You</span></h1>
-      <button className='submit-button'>Submit</button>
+      <h1 className='ty'> Your responses have been Recorded.<span id='tnq'><br></br>Please submit </span></h1>
+      <button className='submit-button' onClick={()=>submitresponse()}>Submit</button>
     </div>
     </>
   )
